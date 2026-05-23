8561d7761648f31eb28358dc42da5fde7180a6a1
Kirill Kondrashov <kondrashovks1989@gmail.com>
2026-05-23 10:54:13 +0200

    Copy proof from Kontorovich

--- SimpleExercises/Continuity.lean
+++ SimpleExercises/Continuity.lean
@@ -1,12 +1,20 @@
 import Mathlib
 
-def RealCont(f: ℝ → ℝ) (c: ℝ) : Prop :=
-  ∀ x : ℝ, ∀ε > 0, ∃δ > 0, |x - c| < δ → |f x - f c| < ε
+def realCont(f : ℝ → ℝ) : Prop :=
+  ∀ x, ∀ ε > 0, ∃ δ > 0, ∀ y, |x - y| < δ → |f x - f y| < ε
 
-theorem realCont_comp(f g: ℝ → ℝ) (c: ℝ)
-  (hg: RealCont g c) (hf: RealCont f (g c)):
-  RealCont (f ∘ g) c := by
-  intro x ε
-  specialize hg
-  specialize hf
-  sorry
+theorem real_cont_comp(f g : ℝ → ℝ)
+  (hg : realCont g) (hf : realCont f):
+  realCont (f ∘ g) := by
+  unfold realCont
+  intro x ε hε
+  specialize hf (g x)
+  specialize hf ε hε
+  choose ξ ξpos hξ using hf
+  specialize hg x ξ ξpos
+  choose δ δpos hδ using hg
+  use δ, δpos
+  intro y ω
+  specialize hδ y ω
+  specialize hξ (g y) hδ
+  exact hξ
